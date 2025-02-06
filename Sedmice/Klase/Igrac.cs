41fa9fc591e37e1d4c596afe19b50b0a31448961
Milos <milos.mandicc@gmail.com>
2025-02-06 21:14:55 +0100

    zavrsena tacka 3

---
+++ Sedmice/Klase/Igrac.cs
@@ -0,0 +1,39 @@
+﻿namespace Klase
+{
+	[Serializable]
+    public class Igrac
+    {
+		private	string ime;
+		private string prezime;
+		private int brojPoena;
+		private int tim;
+
+		public int Tim
+		{
+			get { return tim; }
+			set { tim = value; }
+		}
+
+
+		public int BrojPoena
+		{
+			get { return brojPoena; }
+			set { brojPoena = value; }
+		}
+
+
+		public string Prezime
+		{
+			get { return prezime; }
+			set { prezime = value; }
+		}
+
+
+		public string  Ime
+		{
+			get { return ime; }
+			set { Ime = value; }
+		}
+
+	}
+}
---
+++ Sedmice/Klase/Karta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klase
+{
+    [Serializable]
+	public enum Znak { Tref,Pik,Karo,Herc}
+    public class Karta
+    {
+		private int broj;
+		private Znak znak;
+
+		public Znak Znak
+		{
+			get { return znak; }
+			set { znak = value; }
+		}
+
+		public int Broj
+		{
+			get { return broj; }
+			set { broj = value; }
+		}
+
+	}
+}
